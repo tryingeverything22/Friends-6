dc2061e1bce1d9642097adb863c1ff3fafd6d941
tryingeverything22 <kallikciahmett83@gmail.com>
2025-09-27 10:34:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+
+friends = 10
+# friends = friends + 1
+# friends += 1
+# friends = friends -2
+# friends -= 2
+# friends = friends * 3
+# friends *= 3
+# friends = friends /2
+# friends /= 2
+# friends = friends ** 2
+# friends **=2
+friends %= 3
+print (friends)
